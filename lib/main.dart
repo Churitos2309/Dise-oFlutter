2ce1560b4cd7c6811abf92d63c49f8a93907f30b
Juan Ochoa <loljuanchogaymer@gmail.com>
2024-05-23 10:34:21 -0500

    Nueva vista

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Aplicación de Noticias',
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
@@ -24,97 +24,78 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _currentIndex = 0;
-
-  final List<Widget> _screens = [
-    const InicioScreen(),
-    const DeportesScreen(),
-    // Agregar más pantallas aquí
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Aplicación de Noticias'),
-        leading: Builder(
-          builder: (context) => IconButton(
-            icon: const Icon(Icons.menu),
-            onPressed: () => Scaffold.of(context).openDrawer(),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Navegación con Tabs'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.article),
+                text: 'Noticias',
+              ),
+              Tab(
+                icon: Icon(Icons.message),
+                text: 'Mensajes',
+              ),
+              Tab(
+                icon: Icon(Icons.person),
+                text: 'Perfil',
+              ),
+            ],
           ),
         ),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
+        body: const TabBarView(
           children: [
-            ListTile(
-              title: const Text('Inicio'),
-              onTap: () {
-                _onItemTapped(0);
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: const Text('Deportes'),
-              onTap: () {
-                _onItemTapped(1);
-                Navigator.pop(context);
-              },
-            ),
-            // Agregar más opciones de menú aquí
+            NoticiasTab(),
+            MensajesTab(),
+            PerfilTab(),
           ],
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: _onItemTapped,
-        // ignore: prefer_const_literals_to_create_immutables
-        items: [
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Inicio',
-          ),
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.sports),
-            label: 'Deportes',
-          ),
-          // Agregar más opciones de navegación aquí
-        ],
-      ),
-      body: _screens[_currentIndex],
     );
   }
 }
 
-class InicioScreen extends StatelessWidget {
-  const InicioScreen({super.key});
+class NoticiasTab extends StatelessWidget {
+  const NoticiasTab({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Center(
       child: Text(
-        'Sección: Inicio',
+        'Pestaña de Noticias',
         style: TextStyle(fontSize: 24),
       ),
     );
   }
 }
 
-class DeportesScreen extends StatelessWidget {
-  const DeportesScreen({super.key});
+class MensajesTab extends StatelessWidget {
+  const MensajesTab({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Center(
       child: Text(
-        'Sección: Deportes',
+        'Pestaña de Mensajes',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
+// 
+class PerfilTab extends StatelessWidget {
+  const PerfilTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Pestaña de Perfil',
         style: TextStyle(fontSize: 24),
       ),
     );
